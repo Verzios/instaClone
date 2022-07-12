95b0764f3cd361a4697c8ff87b3379d5c4e902e6
Elias Verginis <eliasver99@gmail.com>
2022-07-12 08:05:47 +0300

    stories

---
+++ src/components/stories.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import '../css/stories.css'
+export default function Stories() {
+  return (
+    <div class="container" className='mx-auto mt-4'>
+        <div class="story-container">
+            <div class="content">
+                <div class="img-container">
+                    <img src="/images/test_upload.jpeg" />
+                </div>
+                <div class="text-container">
+                    Username
+                </div>
+            </div>
+            <div class="content">
+                <div class="img-container">
+                    <img src="/images/test_upload.jpeg" />
+                </div>
+                <div class="text-container">
+                    Username
+                </div>
+            </div>
+            <div class="content">
+                <div class="img-container">
+                    <img src="/images/test_upload.jpeg" />
+                </div>
+                <div class="text-container">
+                    Username
+                </div>
+            </div>
+            <div class="content">
+                <div class="img-container">
+                    <img src="/images/test_upload.jpeg" />
+                </div>
+                <div class="text-container">
+                    Username
+                </div>
+            </div>
+            <div class="content">
+                <div class="img-container">
+                    <img src="/images/test_upload.jpeg" />
+                </div>
+                <div class="text-container">
+                    Username
+                </div>
+            </div>
+            <div class="content">
+                <div class="img-container">
+                    <img src="/images/test_upload.jpeg" />
+                </div>
+                <div class="text-container">
+                    Username
+                </div>
+            </div>
+            <div class="content">
+                <div class="img-container">
+                    <img src="/images/test_upload.jpeg" />
+                </div>
+                <div class="text-container">
+                    Username
+                </div>
+            </div>
+            <div class="content">
+                <div class="img-container">
+                    <img src="/images/test_upload.jpeg" />
+                </div>
+                <div class="text-container">
+                    Username
+                </div>
+            </div>
+            <div class="content">
+                <div class="img-container">
+                    <img src="/images/test_upload.jpeg" />
+                </div>
+                <div class="text-container">
+                    Username
+                </div>
+            </div>
+            <div class="content">
+                <div class="img-container">
+                    <img src="/images/test_upload.jpeg" />
+                </div>
+                <div class="text-container">
+                    Username
+                </div>
+            </div>
+            <div class="content">
+                <div class="img-container">
+                    <img src="/images/test_upload.jpeg" />
+                </div>
+                <div class="text-container">
+                    Username
+                </div>
+            </div>
+            <div class="content">
+                <div class="img-container">
+                    <img src="/images/test_upload.jpeg" />
+                </div>
+                <div class="text-container">
+                    Username
+                </div>
+            </div>
+            <div class="content">
+                <div class="img-container">
+                    <img src="/images/test_upload.jpeg" />
+                </div>
+                <div class="text-container">
+                    Username
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import Stories from '../components/stories'
 import Upload_card from '../components/upload_card'
-
+import '../css/stories.css'
 export default function Home() {
   return (
     <>
     <Navbar />
+    <Stories/>   
     <Upload_card />
     </>
   )
